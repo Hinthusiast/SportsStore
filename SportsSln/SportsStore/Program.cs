cf7e2cf62902591f48d43c7239c533cffbf8dcc2
Hinthusiast <68813392+Hinthusiast@users.noreply.github.com>
2021-09-08 16:03:29 +0800

    initialize services in programs class

--- SportsSln/SportsStore/Program.cs
+++ SportsSln/SportsStore/Program.cs
@@ -1,7 +1,13 @@
+using SportsStore.Models;
+using Microsoft.EntityFrameworkCore;
+
+
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllersWithViews();
 
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<StoreDbContext>(opts => { opts.UseSqlServer("ConnectionStrings:SportsStoreConnection"); });
 
 var app = builder.Build();
 
